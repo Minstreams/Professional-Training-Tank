d2a4e17ab8c16053cc19364aea1f3f58bf313326
chiellini <18319277690@163.com>
2018-06-27 15:10:46 +0800

    动画

---
+++ Assets/Scripts/Function/SimpleAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 简单动画
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+[AddComponentMenu("Function/Simple Animation")]
+public class SimpleAnimation : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    public Sprite[] sprites;
+    public float interval = 0.1f;
+
+    private float timer;
+    private int id;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        id = 0;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            sr.sprite = sprites[id];
+            id++;
+            if (id >= sprites.Length) id = 0;
+            timer = interval;
+        }
+    }
+}
