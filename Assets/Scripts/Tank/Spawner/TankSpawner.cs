58a68ebe8eb615db3930f758303fb100d4b79910
chiellini <18319277690@163.com>
2018-06-27 16:10:41 +0800

    改一个小Bug

--- Assets/Scripts/Tank/Spawner/TankSpawner.cs
+++ Assets/Scripts/Tank/Spawner/TankSpawner.cs
@@ -81,8 +81,11 @@ public abstract class TankSpawner : MonoBehaviour
     private IEnumerator generateTank()
     {
         Transform spawnPoint = spwanPoints[Random.Range(0, spwanPoints.Length)];
-        GameObject g = null;
-        if (spawnPoint != null) g = GameObject.Instantiate(GameSystem.Setter.setting.spwanStar, spawnPoint.position, Quaternion.identity, transform);
+        if (spawnPoint == null)
+        {
+            StopAllCoroutines();
+        }
+        GameObject g = GameObject.Instantiate(GameSystem.Setter.setting.spwanStar, spawnPoint.position, Quaternion.identity, transform);
         yield return new WaitForSeconds(2);
         Destroy(g);
 
